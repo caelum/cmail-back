585c7ddbb1dd6c1d35ea70a99a413e66b567dec7
Mario Souto <soutomarios@gmail.com>
2019-01-07 09:39:20 -0200

    update cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import app from "./src/app";
 
-const port = 3200
+const port = 3200 || process.env.port
+
 app.listen(port, () => {
     console.log(`Servidor subiu na porta ${port}`)
     console.log(`Acesse em: http://localhost:${port}`)
--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import datasource from './infra/datasource';
 import config from './infra/config';
 import consign from 'consign';
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
 
 // Middlewares
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()) // parse application/json
 
